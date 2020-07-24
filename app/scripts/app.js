1d82a56f3e3c4862591168639abf24a14097bd51
Egor Surov <surovegor.se@gmail.com>
2020-07-24 18:58:09 +0400

    main block and customize input

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,19 @@
-let a = 1,
-    b = 10;
 
-console.log("Результат: " + a + b);
+let inputScreen = document.querySelector(".input-screen");
+   
+   updateScreen = () => {
+        let inputRange = document.querySelector(".input-range");
+        document.querySelector(".input-screen").value = inputRange.value;
+        updateColor();
+    }
+    updateRange = () => {
+        let inputRange = document.querySelector(".input-range");
+        inputRange.value = document.querySelector(".input-screen").value;
+        updateColor();
+    }
+    function updateColor() {
+        if (inputScreen.value < 0 || inputScreen.value > 100)
+            inputScreen.style.border = "2px solid red";
+        else 
+            inputScreen.style.border = "2px solid #fff";
+    }
