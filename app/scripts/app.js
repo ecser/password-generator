c63f5c05738e3c2c2448b093fd39ba684527de4a
Egor Surov <surovegor.se@gmail.com>
2020-07-29 15:52:37 +0400

    Правки

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,17 @@
-let inputScreen = document.querySelector(".input-screen");
-   
-   updateScreen = () => {
-        let inputRange = document.querySelector(".input-range");
+const inputScreen = document.querySelector(".input-screen"),
+    inputRange = document.querySelector(".input-range");
+
+    updateScreen = () => {
         document.querySelector(".input-screen").value = inputRange.value;
         updateColor();
     }
+
     updateRange = () => {
-        let inputRange = document.querySelector(".input-range");
         inputRange.value = document.querySelector(".input-screen").value;
         updateColor();
     }
-    function updateColor() {
+
+    updateColor = () => {
         if (inputScreen.value < 0 || inputScreen.value > 100)
             inputScreen.style.border = "2px solid red";
         else 
@@ -23,55 +24,55 @@ let inputScreen = document.querySelector(".input-screen");
           modSymbols = document.querySelector('.symbols'),
           button = document.querySelector('.button-generation');
 
-    let modDataStart = [modUppercase, modLowercase, modNumbers, modSymbols];
-    let modDataFinal = [];
+    let modDataStart = [modUppercase, modLowercase, modNumbers, modSymbols],
+        modDataFinal = [];
 
-    function createArr() {
+    createArr = () => {
         for(let i = 0; i < modDataStart.length; i++) 
             modDataFinal[i] = modDataStart[i].checked ? true : false;
     }
 
-    function buildUppercase() {
+    buildUppercase = () => {
         if(modDataFinal[0] == true) {
             let arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-            let a = Math.floor(Math.random() * arr.length);
-            return arr[a];
+            return arr[Math.floor(Math.random() * arr.length)];
         }
     }
-    function buildLowercase() {
+
+    buildLowercase = () => {
         if(modDataFinal[1] == true) {
             let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-            let a = Math.floor(Math.random() * arr.length);
-            return arr[a];
+            return arr[Math.floor(Math.random() * arr.length)];
         } 
     }
-    function buildNumbers() {
+
+    buildNumbers = () => {
         if(modDataFinal[2] == true) {
             let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-            let a = Math.floor(Math.random() * arr.length);
-            return arr[a];
+            return arr[Math.floor(Math.random() * arr.length)];
         } 
     }
-    function buildSymbols() {
+
+    buildSymbols = () => {
         if(modDataFinal[3] == true) {
             let arr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-            let a = Math.floor(Math.random() * arr.length);
-            return arr[a];
+            return arr[Math.floor(Math.random() * arr.length)];
         } 
     }
     
-    function build() {
-            createArr();
+    const rezult = document.querySelector('.rezult'),
+          error = document.querySelector('.error'),
+          copy = document.querySelector('.copy');
 
-            if(modDataFinal.indexOf(true) != -1) {
+    build = () => {
+        createArr();
 
-            let inputRangeV = document.querySelector(".input-range").value;
-            let arr = [buildUppercase, buildLowercase, buildNumbers, buildSymbols];
-            let randomElement = arr[Math.floor(Math.random() * arr.length)];
-            let password = '';
-            const rezult = document.querySelector('.rezult');
-            const error = document.querySelector('.error');
-      
+        if(modDataFinal.indexOf(true) != -1) {
+            let arr = [buildUppercase, buildLowercase, buildNumbers, buildSymbols],
+                inputRangeV = document.querySelector(".input-range").value,
+                randomElement = arr[Math.floor(Math.random() * arr.length)],
+                password = '';
+
             while(password.length < inputRangeV) {
                 randomElement = arr[Math.floor(Math.random() * arr.length)]();
                 if(randomElement != undefined)
@@ -79,10 +80,7 @@ let inputScreen = document.querySelector(".input-screen");
             }
                 rezult.value = password;
                 error.style.display = 'none';
-        
         } else {
-            const error = document.querySelector('.error');
-            const rezult = document.querySelector('.rezult');
             rezult.value = '';
             error.style.display = 'block';
         }
@@ -90,10 +88,7 @@ let inputScreen = document.querySelector(".input-screen");
 
     button.addEventListener('click', build);
     
-    const copy = document.querySelector('.copy');
-    const rezult = document.querySelector('.rezult');
-    
-    copy.addEventListener('click', function(){
+    copy.addEventListener('click', () => {
         rezult.select();
         document.execCommand('copy');
     });
